f689ef26ade7a90a414210deb11efe940ec6de55
Frank S. Thomas <frank@timepit.eu>
2014-05-18 11:29:29 +0200

    Implement oddmembers

--- Lists.v
+++ Lists.v
@@ -254,20 +254,23 @@ Example test_nonzeros:            nonzeros [0;1;0;2;3;0;0] = [1;2;3].
 Proof. reflexivity.  Qed.
 
 Fixpoint oddmembers (l:natlist) : natlist :=
-  (* FILL IN HERE *) admit.
+  match l with
+  | nil => nil
+  | h :: t => if oddb h then h :: oddmembers t else oddmembers t
+  end.
 
 Example test_oddmembers:            oddmembers [0;1;0;2;3;0;0] = [1;3].
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity.  Qed.
 
 Fixpoint countoddmembers (l:natlist) : nat :=
-  (* FILL IN HERE *) admit.
+  length (oddmembers l).
 
 Example test_countoddmembers1:    countoddmembers [1;0;3;1;4;5] = 4.
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity.  Qed.
 Example test_countoddmembers2:    countoddmembers [0;2;4] = 0.
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity.  Qed.
 Example test_countoddmembers3:    countoddmembers nil = 0.
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity.  Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, advanced (alternate) *)
